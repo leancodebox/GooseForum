1c382064db9805c266be4b1c69e880299695dee4
eatmeatball <hzjmessage@outlook.com>
2024-05-14 23:00:00 +0800

    main:Create browsercache.go

---
+++ app/http/middleware/browsercache.go
@@ -0,0 +1,16 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func BrowserCache(c *gin.Context) {
+	// 如果请求的是 index.html，禁用浏览器缓存
+	if c.Request.URL.Path == "/actor/" {
+		c.Header("Cache-Control", "no-store")
+	} else {
+		// 其他情况下继续使用浏览器缓存
+		c.Header("Cache-Control", "public, max-age=3600")
+	}
+	c.Next()
+}
