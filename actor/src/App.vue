ebf715fda31d6c005090d4152fbb3a9e38b41c32
eatmeatball <hzjmessage@outlook.com>
2025-01-02 23:00:00 +0800

    main:fix

--- actor/src/App.vue
+++ actor/src/App.vue
@@ -20,6 +20,7 @@ import {RouterLink} from "vue-router";
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 
 
+// 全局浮动按钮的下拉菜单选项
 const options = [
   {
     label: 'super menu',
@@ -69,23 +70,29 @@ function handleSelect(key) {
 </script>
 
 <template>
+  <!-- 全局通知提供者，限制最大显示数量为3 -->
   <n-notification-provider :max="3">
-  <n-message-provider>
-    <n-loading-bar-provider>
-      <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
-        <n-layout position="absolute" content-style="width: 100%;height: 100%;">
-          <router-view></router-view>
-        </n-layout>
-        <n-dropdown trigger="hover" :options="options" @select="handleSelect" :size="'huge'">
-          <n-float-button  circle :right="56" :bottom="30" style="z-index:1501">
-                <n-icon>
-                  <sparkles-outline/>
-                </n-icon>
-          </n-float-button>
-        </n-dropdown>
-      </n-config-provider>
-    </n-loading-bar-provider>
-  </n-message-provider>
+    <!-- 全局消息提供者 -->
+    <n-message-provider>
+      <!-- 全局加载条提供者 -->
+      <n-loading-bar-provider>
+        <!-- 全局配置提供者，设置中文语言 -->
+        <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+          <!-- 主布局容器 -->
+          <n-layout position="absolute" content-style="width: 100%;height: 100%;">
+            <router-view></router-view>
+          </n-layout>
+          <!-- 全局浮动按钮 -->
+          <n-dropdown trigger="hover" :options="options" @select="handleSelect" :size="'huge'">
+            <n-float-button circle :right="56" :bottom="30" style="z-index:1501">
+              <n-icon>
+                <sparkles-outline/>
+              </n-icon>
+            </n-float-button>
+          </n-dropdown>
+        </n-config-provider>
+      </n-loading-bar-provider>
+    </n-message-provider>
   </n-notification-provider>
 </template>
 
