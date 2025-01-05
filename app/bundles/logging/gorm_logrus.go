fd95fa391ec5832cb021e8197677be1b833c7a7e
eatmeatball <abandon1a2b@outlook.com>
2025-01-05 23:00:00 +0800

    日志

--- app/bundles/logging/gorm_logrus.go
+++ app/bundles/logging/gorm_logrus.go
@@ -4,21 +4,18 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/spf13/cast"
+	"gorm.io/gorm/utils"
 	"log/slog"
 	"time"
 
-	"gorm.io/gorm"
 	gormLogger "gorm.io/gorm/logger"
 )
 
 // GormLogger 操作对象，实现 gormLogger.Interface
 type GormLogger struct {
-	Config        GormLoggerConfig
+	Config        gormLogger.Config
 	SlowThreshold time.Duration
-}
-
-type GormLoggerConfig struct {
-	LogLevel gormLogger.LogLevel
 }
 
 // NewGormLogger 外部调用。实例化一个 GormLogger 对象，示例：
@@ -26,79 +23,99 @@ type GormLoggerConfig struct {
 //	DB, err := gorm.Open(dbConfig, &gorm.Config{
 //		Logger: logger.NewGormLogger(),
 //	})
-func NewGormLogger() GormLogger {
+func NewGormLogger() *GormLogger {
 	logLevel := gormLogger.Warn
 	if debug {
 		logLevel = gormLogger.Info
 	}
-	return GormLogger{
-		Config: GormLoggerConfig{
-			LogLevel: logLevel,
+	return &GormLogger{
+		Config: gormLogger.Config{
+			LogLevel:                  logLevel,
+			IgnoreRecordNotFoundError: true,
 		},
 		SlowThreshold: 500 * time.Millisecond, // 慢查询阈值，单位为千分之一秒
 	}
 }
 
 // LogMode 实现 gormLogger.Interface 的 LogMode 方法
-func (l GormLogger) LogMode(level gormLogger.LogLevel) gormLogger.Interface {
-	newlogger := l
+func (l *GormLogger) LogMode(level gormLogger.LogLevel) gormLogger.Interface {
+	newlogger := *l
 	newlogger.Config.LogLevel = level
 	return &newlogger
 }
 
 // Info 实现 gormLogger.Interface 的 Info 方法
-func (l GormLogger) Info(ctx context.Context, format string, args ...any) {
+func (l *GormLogger) Info(ctx context.Context, format string, args ...any) {
 	if l.Config.LogLevel >= gormLogger.Info {
 		slog.Info(fmt.Sprintf(format, args...))
 	}
 }
 
 // Warn 实现 gormLogger.Interface 的 Warn 方法
-func (l GormLogger) Warn(ctx context.Context, format string, args ...any) {
+func (l *GormLogger) Warn(ctx context.Context, format string, args ...any) {
 	if l.Config.LogLevel >= gormLogger.Warn {
 		slog.Warn(fmt.Sprintf(format, args...))
 	}
 }
 
 // Error 实现 gormLogger.Interface 的 Error 方法
-func (l GormLogger) Error(ctx context.Context, format string, args ...any) {
+func (l *GormLogger) Error(ctx context.Context, format string, args ...any) {
 	if l.Config.LogLevel >= gormLogger.Error {
 		slog.Error(fmt.Sprintf(format, args...))
 	}
 }
 
-// Trace 实现 gormLogger.Interface 的 Trace 方法
-func (l GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+func (l *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
 	if l.Config.LogLevel <= gormLogger.Silent {
-		if debug {
-			elapsed := time.Since(begin)
-			sql, rows := fc()
-			slog.Debug("Database Query", slog.Group(
-				"gorm",
-				"sql", sql,
-				"time", fmt.Sprintf("%.3fms", float64(elapsed.Nanoseconds())/1e6),
-				"rows", rows,
-			))
-		}
 		return
 	}
-	// 获取运行时间
 	elapsed := time.Since(begin)
-	sql, rows := fc()
 	switch {
-	case err != nil && (!errors.Is(err, gorm.ErrRecordNotFound)):
-		slog.Warn("Database ErrRecordNotFound", slog.Group(
-			"gorm",
+	case err != nil && l.Config.LogLevel >= gormLogger.Error && (!errors.Is(err, gormLogger.ErrRecordNotFound) || !l.Config.IgnoreRecordNotFoundError):
+		sql, rows := fc()
+		row := cast.ToString(rows)
+		if rows == -1 {
+			row = "-"
+		}
+		slog.Warn("gormLogger",
+			"fileWithLineNum", utils.FileWithLineNum(),
+			"err", err,
+			"elapsed", float64(elapsed.Nanoseconds())/1e6,
+			"row", row,
+			"sql", sql,
+		)
+	case elapsed > l.SlowThreshold && l.SlowThreshold != 0 && l.Config.LogLevel >= gormLogger.Warn:
+		sql, rows := fc()
+		slowLog := fmt.Sprintf("SLOW SQL >= %v", l.SlowThreshold)
+		row := cast.ToString(rows)
+		if rows == -1 {
+			row = "-"
+		}
+		slog.Warn("gormLogger",
+			"fileWithLineNum", utils.FileWithLineNum(),
+			"slowLog", slowLog,
+			"elapsed", float64(elapsed.Nanoseconds())/1e6,
+			"row", row,
+			"sql", sql,
+		)
+	case l.Config.LogLevel == gormLogger.Info:
+		sql, rows := fc()
+		row := cast.ToString(rows)
+		if rows == -1 {
+			row = "-"
+		}
+		slog.Warn("gormLogger",
+			"fileWithLineNum", utils.FileWithLineNum(),
+			"elapsed", float64(elapsed.Nanoseconds())/1e6,
+			"row", row,
+			"sql", sql,
+		)
+	case debug:
+		sql, rows := fc()
+		slog.Debug("gormLogger",
 			"sql", sql,
 			"time", fmt.Sprintf("%.3fms", float64(elapsed.Nanoseconds())/1e6),
 			"rows", rows,
-		))
-	case l.SlowThreshold != 0 && elapsed > l.SlowThreshold:
-		slog.Warn("Database Slow Log", slog.Group(
-			"gorm",
-			"sql", sql,
-			"time", fmt.Sprintf("%.3fms", float64(elapsed.Nanoseconds())/1e6),
-		))
+		)
 	}
-
 }
