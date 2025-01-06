e8efbdce0d257853581742e205ce81c5fbab66c4
eatmeatball <hzjmessage@outlook.com>
2025-01-06 23:00:00 +0800

    main:fix

--- actor/src/pages/manager/AllTool.vue
+++ actor/src/pages/manager/AllTool.vue
@@ -1,7 +1,6 @@
 <script setup>
-import {NButton, NCard, NGrid, NGridItem, NIcon, NSpace, NStatistic, useMessage} from 'naive-ui'
+import {NButton, NCard, NGrid, NGridItem, NIcon, NStatistic, useMessage} from 'naive-ui'
 import {getUserInfo} from "@/service/request"
-import CpTool from "@/pages/manager/tool/CpTool.vue";
 
 const message = useMessage()
 
@@ -12,32 +11,53 @@ function getUserInfoAction() {
 }
 </script>
 <template>
-  <n-space vertical>
+  <n-grid :cols="24">
+    <n-grid-item :span="12">
 
-    <n-card title="统计数据">
-      <n-grid>
-        <n-grid-item :span="12">
-          <n-statistic label="文章/总数(包含草稿)" :value="99">
-            <template #prefix>
-              <n-icon>
+      <n-card title="统计数据">
+        <n-grid :cols="24">
+          <n-grid-item :span="8">
+            <n-statistic label="文章/总数(包含草稿)" :value="99">
+              <template #prefix>
+                <n-icon>
 
-              </n-icon>
-            </template>
-            <template #suffix>/ 100</template>
-          </n-statistic>
-        </n-grid-item>
-        <n-grid-item :span="12">
-          <n-statistic label="用户数量">1,234,123</n-statistic>
-        </n-grid-item>
-      </n-grid>
-    </n-card>
-    <n-card title="复制">
-      <cp-tool></cp-tool>
-    </n-card>
-    <n-card title="查看当前用户信息">
-      <n-button @click="getUserInfoAction"> 获取用户信息</n-button>
-    </n-card>
-  </n-space>
+                </n-icon>
+              </template>
+              <template #suffix>/ 100</template>
+            </n-statistic>
+          </n-grid-item>
+          <n-grid-item :span="8">
+            <n-statistic label="用户数量">1,234,123</n-statistic>
+          </n-grid-item>
+          <n-grid-item :span="8">
+            <n-statistic label="回复量">1,234,123</n-statistic>
+          </n-grid-item>
+        </n-grid>
+      </n-card>
+    </n-grid-item>
+
+    <n-grid-item :span="12">
+      <n-card title="语言包">
+        <n-grid :cols="24">
+          <n-grid-item :span="8">
+            简体中文
+          </n-grid-item>
+          <n-grid-item :span="8">
+            简体中文
+          </n-grid-item>
+          <n-grid-item :span="8">
+            简体中文
+          </n-grid-item>
+        </n-grid>
+      </n-card>
+    </n-grid-item>
+    <n-grid-item :span="12">
+      <n-card title="功能模块">
+        <n-button @click="getUserInfoAction">获取用户信息</n-button>
+      </n-card>
+    </n-grid-item>
+  </n-grid>
+
 </template>
 <style>
 .carousel-img {
