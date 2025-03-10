dd3510ec74c58ba762736186c1f20892753d09aa
eatmeatball <hzjmessage@outlook.com>
2025-03-10 23:00:00 +0800

    main:Update MarkdownEdit.vue

--- actorV2/src/components/MarkdownEdit.vue
+++ actorV2/src/components/MarkdownEdit.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script setup>
+// vue-shim.d.ts 如果替换，考虑去掉这个。
 import { defineProps, toRefs } from 'vue';
 import {mavonEditor} from 'mavon-editor';
 import 'mavon-editor/dist/css/index.css';
