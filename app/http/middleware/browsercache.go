73b061747da25231811a793f73448dacee0ecdaa
eatmeatball <abandon1a2b@outlook.com>
2025-03-11 23:00:00 +0800

    1

--- app/http/middleware/browsercache.go
+++ app/http/middleware/browsercache.go
@@ -2,14 +2,15 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
-	"strings"
 )
 
 func BrowserCache(c *gin.Context) {
 	// 如果请求的是 index.html，禁用浏览器缓存
 	if c.Request.URL.Path == "/actor/" {
 		c.Header("Cache-Control", "no-store")
-	} else if strings.HasPrefix(c.Request.RequestURI, "/app") {
+	} else if c.Request.URL.Path == "/app/" {
+		c.Header("Cache-Control", "no-store")
+	} else if c.Request.URL.Path == "/app/admin" {
 		c.Header("Cache-Control", "no-store")
 	} else {
 		// 其他情况下继续使用浏览器缓存
