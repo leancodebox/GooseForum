9ef9f8ea0dcc356ef2a504f0c02075b7dca68773
eatmeatball <abandon1a2b@outlook.com>
2025-04-02 22:11:16 +0800

    Update articleService.ts

--- actorV2/src/utils/articleService.ts
+++ actorV2/src/utils/articleService.ts
@@ -53,32 +53,32 @@ export const getUserInfo = async () => {
 
 
 // 获取通知列表
-export function getNotificationList(page:any, pageSize:any, unreadOnly:any):Promise<Result<PageData<Notifications>>> {
+export function getNotificationList(page: any, pageSize: any, unreadOnly: any): Promise<Result<PageData<Notifications>>> {
     return axiosInstance.post('/bbs/notification/list', {
-        page:page,
-        pageSize:pageSize,
-        unreadOnly:unreadOnly,
+        page: page,
+        pageSize: pageSize,
+        unreadOnly: unreadOnly,
     })
 }
 
 // 获取未读通知数量
-export function getUnreadCount():Promise<Result<any>> {
+export function getUnreadCount(): Promise<Result<any>> {
     return axiosInstance.get('/bbs/notification/unread-count')
 }
 
 // 标记通知为已读
-export function markAsRead(notificationId: any):Promise<Result<any>> {
+export function markAsRead(notificationId: any): Promise<Result<any>> {
     return axiosInstance.post('/bbs/notification/mark-read', {
-        notificationId:notificationId
+        notificationId: notificationId
     })
 }
 
 // 标记所有通知为已读
-export function markAllAsRead():Promise<Result<any>> {
+export function markAllAsRead(): Promise<Result<any>> {
     return axiosInstance.post('/bbs/notification/mark-all-read')
 }
 
-export function uploadAvatar(file:Blob):Promise<Result<any>> {
+export function uploadAvatar(file: Blob): Promise<Result<any>> {
     const formData = new FormData();
     // 如果是 Blob 对象，需要创建 File 对象
     if (file instanceof Blob) {
@@ -92,4 +92,21 @@ export function uploadAvatar(file:Blob):Promise<Result<any>> {
             'Content-Type': 'multipart/form-data'
         }
     });
+}
+
+
+export function saveUserInfo(
+    nickname: String,
+    email: String,
+    bio: String,
+    signature: String,
+    website: String,
+): Promise<Result<any>> {
+    return axiosInstance.post('set-user-info', {
+        nickname: nickname,
+        email: email,
+        bio: bio,
+        signature: signature,
+        website: website,
+    })
 }
