a4be5d34e9190726d1326c92af41d6e5d7ce1ed1
eatmeatball <abandon1a2b@outlook.com>
2025-04-14 22:26:43 +0800

    dev:init set

---
+++ app/migration/initBlog.md
@@ -0,0 +1,5 @@
+# hi 欢迎使用 GooseForum 管理你的社区
+
+[GooseForum](https://github.com/leancodebox/GooseForum) 是一个基于 [Go](https://golang.org/) 的论坛程序。  
+
+现在你可以开始你的社区建设了～。 
--- app/migration/migration.go
+++ app/migration/migration.go
@@ -1,9 +1,13 @@
 package migration
 
 import (
+	_ "embed"
+	"fmt"
 	"github.com/leancodebox/GooseForum/app/bundles/connect/db4fileconnect"
 	"github.com/leancodebox/GooseForum/app/bundles/connect/dbconnect"
 	"github.com/leancodebox/GooseForum/app/bundles/setting"
+	"github.com/leancodebox/GooseForum/app/http/controllers"
+	"github.com/leancodebox/GooseForum/app/http/controllers/component"
 	"github.com/leancodebox/GooseForum/app/models/filemodel/filedata"
 	"github.com/leancodebox/GooseForum/app/models/forum/articleCategory"
 	"github.com/leancodebox/GooseForum/app/models/forum/articleCategoryRs"
@@ -20,12 +24,14 @@ import (
 	"github.com/leancodebox/GooseForum/app/models/forum/userPoints"
 	"github.com/leancodebox/GooseForum/app/models/forum/userRoleRs"
 	"github.com/leancodebox/GooseForum/app/models/forum/users"
+	"github.com/leancodebox/GooseForum/app/service/permission"
 	"log/slog"
 )
 
 func M() {
 	// 数据库迁移
 	migration(setting.UseMigration())
+	dataInit()
 }
 
 func migration(migration bool) {
@@ -77,3 +83,72 @@ func migration(migration bool) {
 		}
 	}
 }
+
+func dataInit() {
+	user, _ := users.Get(1)
+	if user.Id == 1 {
+		return
+	}
+	adminUser := users.MakeUser("admin", "gooseforum", "admin@email.com")
+
+	if err := users.Create(adminUser); err != nil {
+		slog.Info("初始化用户失败")
+		return
+	}
+
+	roleEntity := role.Get(1)
+	if roleEntity.Id == 0 {
+		roleEntity.RoleName = "管理员"
+		roleEntity.Effective = 1
+		if err := role.SaveOrCreateById(&roleEntity); err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("角色不存在，创建角色")
+	}
+
+	rp := rolePermissionRs.GetRsByRoleIdAndPermission(roleEntity.Id, permission.Admin.Id())
+	if rp.Id == 0 {
+		rp.RoleId = roleEntity.Id
+		rp.PermissionId = permission.Admin.Id()
+		rp.Effective = 1
+		rolePermissionRs.SaveOrCreateById(&rp)
+		fmt.Println("角色权限关系不存在，创建角色权限关系")
+	}
+
+	ur := userRoleRs.GetByUserIdAndRoleId(adminUser.Id, roleEntity.Id)
+	if ur.Id == 0 {
+		ur.RoleId = roleEntity.Id
+		ur.UserId = adminUser.Id
+		ur.Effective = 1
+		if err := userRoleRs.SaveOrCreateById(&ur); err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("用户角色关系不存在，创建用户角色关系")
+	}
+
+	category := articleCategory.Get(1)
+	if category.Id == 0 {
+		category.Category = "GooseForum"
+		articleCategory.SaveOrCreateById(&category)
+		fmt.Println("标签不存在，创建标签")
+	}
+	controllers.WriteArticles(component.BetterRequest[controllers.WriteArticleReq]{
+		Params: controllers.WriteArticleReq{
+			Id:         0,
+			Content:    initBlog,
+			Title:      "Hi With GooseForum",
+			Type:       1,
+			CategoryId: []uint64{1},
+		},
+		UserId: 0,
+	})
+
+	// 是否有用户
+	// 是否有文章类别
+	// 是否用文章
+}
+
+//go:embed initBlog.md
+var initBlog string
