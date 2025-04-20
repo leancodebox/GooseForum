1cf4f0220f9ba9f773913b186d10a9dd4efa122e
eatmeatball <abandon1a2b@outlook.com>
2025-04-20 21:59:53 +0800

    dev:friendship

---
+++ app/models/forum/friendshipLinks/friendshipLinks_connect.go
@@ -0,0 +1,24 @@
+package friendshipLinks
+
+import (
+	"gorm.io/gorm"
+
+	db "github.com/leancodebox/GooseForum/app/bundles/connect/dbconnect"
+)
+
+// Prohibit manual changes
+// 禁止手动更改本文件
+
+func builder() *gorm.DB {
+	return db.Connect().Table(tableName)
+}
+
+func first(db *gorm.DB) (el *Entity) {
+	db.First(&el)
+	return
+}
+
+func getList(db *gorm.DB) (el []*Entity) {
+	db.Find(&el)
+	return
+}
