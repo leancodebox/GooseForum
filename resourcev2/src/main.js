168c75756cb476c6793864f9d0d15fcd3d57304a
eatmeatball <abandon1a2b@outlook.com>
2025-06-05 21:16:06 +0800

    dev:fix

--- resourcev2/src/main.js
+++ resourcev2/src/main.js
@@ -2,5 +2,5 @@ import {createApp} from 'vue'
 import Main from './main.vue'
 import './style.css'
 
-const app = createApp(Main)
-app.mount('#app')
+// const app = createApp(Main)
+// app.mount('#app')
