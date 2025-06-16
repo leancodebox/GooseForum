3503f658578a381d882ffed62a8f0722b489db65
abandon1a2b <abandon1a2b@outlook.com>
2025-06-16 23:25:18 +0800

    索引

--- app/models/forum/users/users.go
+++ app/models/forum/users/users.go
@@ -54,25 +54,25 @@ const fieldUpdatedAt = "updated_at"
 const fieldDeletedAt = "deleted_at"
 
 type Entity struct {
-	Id                  uint64     `gorm:"primaryKey;column:id;autoIncrement;not null;" json:"id"`                     //
-	Username            string     `gorm:"column:username;type:varchar(255);not null;default:'';" json:"username"`     //
-	Nickname            string     `gorm:"column:nickname;type:varchar(255);not null;default:'';" json:"nickname"`     //
-	Email               string     `gorm:"column:email;type:varchar(255);not null;default:'';" json:"email"`           //
-	Password            string     `gorm:"column:password;type:varchar(255);not null;default:'';" json:"password"`     //
-	MobileAreaCode      string     `gorm:"column:mobile_area_code;type:varchar(16);" json:"mobileAreaCode"`            //
-	MobilePhoneNumber   string     `gorm:"column:mobile_phone_number;type:varchar(64);" json:"mobilePhoneNumber"`      //
-	RoleId              uint64     `gorm:"column:role_id;type:bigint unsigned;not null;default:0;" json:"roleId"`      //
-	Status              int8       `gorm:"column:status;type:tinyint;not null;default:0;" json:"status"`               // 状态：0正常 1冻结
-	Validate            int8       `gorm:"column:validate;type:tinyint;not null;default:0;" json:"validate"`           // 是否验证通过: 0未通过/未验证 1 验证通过
-	ActivatedAt         time.Time  `gorm:"column:activated_at;type:datetime;" json:"activatedAt"`                      // 激活时间
-	Prestige            int64      `gorm:"column:prestige;type:bigint;not null;default:0;" json:"prestige"`            // 声望
-	AvatarUrl           string     `gorm:"column:avatar_url;type:varchar(255);" json:"avatarUrl"`                      // 头像URL
-	Bio                 string     `gorm:"column:bio;type:varchar(500);" json:"bio"`                                   // 个人简介
-	Signature           string     `gorm:"column:signature;type:varchar(255);" json:"signature"`                       // 署名
-	Website             string     `gorm:"column:website;type:varchar(255);" json:"website"`                           // 个人网站
-	WebsiteName         string     `gorm:"column:website_name;type:varchar(64);" json:"websiteName"`                   // 个人网站名
-	ExternalInformation string     `gorm:"column:external_information;type:varchar(2048);" json:"externalInformation"` // 外部信息
-	CreatedAt           time.Time  `gorm:"column:created_at;index;autoCreateTime;<-:create;" json:"createdAt"`         //
+	Id                  uint64     `gorm:"primaryKey;column:id;autoIncrement;not null;" json:"id"`                       //
+	Username            string     `gorm:"column:username;index;type:varchar(255);not null;default:'';" json:"username"` //
+	Nickname            string     `gorm:"column:nickname;type:varchar(255);not null;default:'';" json:"nickname"`       //
+	Email               string     `gorm:"column:email;index;type:varchar(255);not null;default:'';" json:"email"`       //
+	Password            string     `gorm:"column:password;type:varchar(255);not null;default:'';" json:"password"`       //
+	MobileAreaCode      string     `gorm:"column:mobile_area_code;type:varchar(16);" json:"mobileAreaCode"`              //
+	MobilePhoneNumber   string     `gorm:"column:mobile_phone_number;type:varchar(64);" json:"mobilePhoneNumber"`        //
+	RoleId              uint64     `gorm:"column:role_id;type:bigint unsigned;not null;default:0;" json:"roleId"`        //
+	Status              int8       `gorm:"column:status;type:tinyint;not null;default:0;" json:"status"`                 // 状态：0正常 1冻结
+	Validate            int8       `gorm:"column:validate;type:tinyint;not null;default:0;" json:"validate"`             // 是否验证通过: 0未通过/未验证 1 验证通过
+	ActivatedAt         time.Time  `gorm:"column:activated_at;type:datetime;" json:"activatedAt"`                        // 激活时间
+	Prestige            int64      `gorm:"column:prestige;type:bigint;not null;default:0;" json:"prestige"`              // 声望
+	AvatarUrl           string     `gorm:"column:avatar_url;type:varchar(255);" json:"avatarUrl"`                        // 头像URL
+	Bio                 string     `gorm:"column:bio;type:varchar(500);" json:"bio"`                                     // 个人简介
+	Signature           string     `gorm:"column:signature;type:varchar(255);" json:"signature"`                         // 署名
+	Website             string     `gorm:"column:website;type:varchar(255);" json:"website"`                             // 个人网站
+	WebsiteName         string     `gorm:"column:website_name;type:varchar(64);" json:"websiteName"`                     // 个人网站名
+	ExternalInformation string     `gorm:"column:external_information;type:varchar(2048);" json:"externalInformation"`   // 外部信息
+	CreatedAt           time.Time  `gorm:"column:created_at;index;autoCreateTime;<-:create;" json:"createdAt"`           //
 	UpdatedAt           time.Time  `gorm:"column:updated_at;autoUpdateTime;" json:"updatedAt"`
 	DeletedAt           *time.Time `gorm:"column:deleted_at;type:datetime;" json:"deletedAt"` //
 }
