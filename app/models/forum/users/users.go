c5b8162f8f56368542b4bc54611a5730041be436
abandon1a2b <abandon1a2b@outlook.com>
2025-07-12 15:30:16 +0800

    Update users.go

--- app/models/forum/users/users.go
+++ app/models/forum/users/users.go
@@ -1,13 +1,9 @@
 package users
 
 import (
-	"database/sql/driver"
-	"errors"
-	"fmt"
 	"strings"
 	"time"
 
-	"github.com/leancodebox/GooseForum/app/bundles/jsonopt"
 	"github.com/leancodebox/GooseForum/app/service/urlconfig"
 
 	"github.com/leancodebox/GooseForum/app/bundles/algorithm"
@@ -67,23 +63,6 @@ type ExternalInformation struct {
 	Zhihu    ExternalInformationItem `json:"zhihu"`
 }
 
-func (itself *ExternalInformation) Value() (driver.Value, error) {
-	return jsonopt.EncodeE(itself)
-}
-
-func (itself *ExternalInformation) Scan(value any) error {
-	bytes, ok := value.(string)
-	if !ok {
-		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
-	}
-	if len(bytes) == 0 {
-		return nil
-	}
-	r, err := jsonopt.DecodeE[ExternalInformation](bytes)
-	*itself = r
-	return err
-}
-
 type EntityComplete struct {
 	// base
 	Id          uint64     `gorm:"primaryKey;column:id;autoIncrement;not null;" json:"id"`                      //
@@ -95,15 +74,15 @@ type EntityComplete struct {
 	ActivatedAt *time.Time `gorm:"column:activated_at;type:datetime;" json:"activatedAt"`                       // 激活时间
 
 	// info
-	Nickname            string              `gorm:"column:nickname;type:varchar(64);not null;default:'';" json:"nickname"`                  //
-	RoleId              uint64              `gorm:"column:role_id;type:bigint unsigned;not null;default:0;" json:"roleId"`                  //
-	Prestige            int64               `gorm:"column:prestige;type:bigint;not null;default:0;" json:"prestige"`                        // 声望
-	AvatarUrl           string              `gorm:"column:avatar_url;type:varchar(255);" json:"avatarUrl"`                                  // 头像URL
-	Bio                 string              `gorm:"column:bio;type:varchar(500);not null;default:'';" json:"bio"`                           // 个人简介
-	Signature           string              `gorm:"column:signature;type:varchar(255);not null;default:'';" json:"signature"`               // 署名
-	WebsiteName         string              `gorm:"column:website_name;type:varchar(64);not null;default:'';" json:"websiteName"`           // 个人网站名
-	Website             string              `gorm:"column:website;type:varchar(255);not null;default:'';" json:"website"`                   // 个人网站
-	ExternalInformation ExternalInformation `gorm:"column:external_information;type:varchar(2048);default:'{}'" json:"externalInformation"` // 外部信息
+	Nickname            string              `gorm:"column:nickname;type:varchar(64);not null;default:'';" json:"nickname"`                                  //
+	RoleId              uint64              `gorm:"column:role_id;type:bigint unsigned;not null;default:0;" json:"roleId"`                                  //
+	Prestige            int64               `gorm:"column:prestige;type:bigint;not null;default:0;" json:"prestige"`                                        // 声望
+	AvatarUrl           string              `gorm:"column:avatar_url;type:varchar(255);" json:"avatarUrl"`                                                  // 头像URL
+	Bio                 string              `gorm:"column:bio;type:varchar(500);not null;default:'';" json:"bio"`                                           // 个人简介
+	Signature           string              `gorm:"column:signature;type:varchar(255);not null;default:'';" json:"signature"`                               // 署名
+	WebsiteName         string              `gorm:"column:website_name;type:varchar(64);not null;default:'';" json:"websiteName"`                           // 个人网站名
+	Website             string              `gorm:"column:website;type:varchar(255);not null;default:'';" json:"website"`                                   // 个人网站
+	ExternalInformation ExternalInformation `gorm:"column:external_information;type:varchar(2048);default:'{}';serializer:json" json:"externalInformation"` // 外部信息
 
 	// status
 	CreatedAt time.Time  `gorm:"column:created_at;index;autoCreateTime;<-:create;" json:"createdAt"` //
