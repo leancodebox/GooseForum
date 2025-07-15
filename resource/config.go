3b1d809b88706effa3dd5008cce8d95924739d0a
abandon1a2b <abandon1a2b@outlook.com>
2025-07-15 22:35:36 +0800

    dev:docs

--- resource/config.go
+++ resource/config.go
@@ -2,6 +2,7 @@ package resource
 
 import (
 	"embed"
+	"errors"
 	"fmt"
 	"html/template"
 	"io/fs"
@@ -43,6 +44,20 @@ func GetTemplates(globalFunc template.FuncMap) *template.Template {
 			},
 			"IsOnline": func(t time.Time) bool {
 				return time.Now().Sub(t) < 120*time.Second
+			},
+			"dict": func(values ...interface{}) (map[string]interface{}, error) {
+				if len(values)%2 != 0 {
+					return nil, errors.New("invalid dict call")
+				}
+				dict := make(map[string]interface{}, len(values)/2)
+				for i := 0; i < len(values); i += 2 {
+					key, ok := values[i].(string)
+					if !ok {
+						return nil, errors.New("dict keys must be strings")
+					}
+					dict[key] = values[i+1]
+				}
+				return dict, nil
 			},
 		}).
 		Funcs(globalFunc)
