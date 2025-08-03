acf004df725bd2854c1955e14546852e99d5ff18
abandon1a2b <abandon1a2b@outlook.com>
2025-08-03 18:31:27 +0800

    clean code

--- app/bundles/asyncwrite/asyncLumberJack.go
+++ app/bundles/asyncwrite/asyncLumberJack.go
@@ -2,7 +2,6 @@ package asyncwrite
 
 import (
 	"bufio"
-	"bytes"
 	"context"
 	"fmt"
 	"gopkg.in/natefinch/lumberjack.v2"
@@ -12,57 +11,6 @@ import (
 type AsyncW struct {
 	dataChan    chan []byte
 	closeFinish chan bool
-}
-
-func AsyncLumberjack(io *lumberjack.Logger) *AsyncW {
-	r := &AsyncW{
-		dataChan:    make(chan []byte, 256),
-		closeFinish: make(chan bool),
-	}
-	maxLen := 1024 * 8
-	w := io
-	go func() {
-		defer func() {
-			if err := w.Close(); err != nil {
-				fmt.Println(err)
-			}
-			r.closeFinish <- true
-			return
-		}()
-		ticker := time.NewTicker(time.Millisecond * 100)
-		defer ticker.Stop()
-		var buf bytes.Buffer
-		for {
-			select {
-			case val, ok := <-r.dataChan:
-				if !ok {
-					if buf.Len() > 0 {
-						if _, err := w.Write(buf.Bytes()); err != nil {
-							fmt.Println(err)
-						}
-						buf.Reset()
-					}
-					return
-				}
-
-				buf.Write(val)
-				if buf.Len() >= maxLen {
-					if _, err := w.Write(buf.Bytes()); err != nil {
-						fmt.Println(err)
-					}
-					buf.Reset()
-				}
-			case <-ticker.C:
-				if buf.Len() > 0 {
-					if _, err := w.Write(buf.Bytes()); err != nil {
-						fmt.Println(err)
-					}
-					buf.Reset()
-				}
-			}
-		}
-	}()
-	return r
 }
 
 func AsyncLumberjackBufIo(io *lumberjack.Logger) *AsyncW {
