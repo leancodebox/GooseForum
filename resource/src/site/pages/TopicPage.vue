2b220073d1f672f3a009bc1e11d7a37093702fc6
abandon1a2b <abandon1a2b@outlook.com>
2026-07-08 20:13:58 +0800

    fix: flatten delete post dialog preview

--- resource/src/site/pages/TopicPage.vue
+++ resource/src/site/pages/TopicPage.vue
@@ -1741,9 +1741,9 @@ async function removePost(postId: number) {
               </button>
             </div>
 
-            <div class="mt-4 rounded-md border border-line bg-base-200 px-3 py-2">
-              <div class="text-xs font-semibold text-base-content/55">@{{ pendingDeletePost.author.username }}</div>
-              <p class="mt-1 line-clamp-3 whitespace-pre-wrap text-sm leading-6 text-base-content/75">{{ pendingDeletePost.content }}</p>
+            <div class="mt-4 border-l-2 border-error/35 pl-3">
+              <div class="text-xs font-semibold text-base-content/45">@{{ pendingDeletePost.author.username }}</div>
+              <p class="mt-1 line-clamp-3 whitespace-pre-wrap text-sm leading-6 text-base-content/70">{{ pendingDeletePost.content }}</p>
             </div>
 
             <p v-if="deleteErrorMessage" class="mt-3 text-sm text-error">{{ deleteErrorMessage }}</p>
